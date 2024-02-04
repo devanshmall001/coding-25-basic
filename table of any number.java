151f4dda441e3519f80328170d5a288f94f09d0a
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2024-02-04 20:44:15 +0530

    Create table of any number.java

---
+++ table of any number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            int prd=a*i;
+        System.out.println(prd);
+        }
+    }
+}
